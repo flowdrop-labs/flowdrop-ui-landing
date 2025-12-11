46d69ad544dce6fc9088b4d6e3b82ec3c3f31d57
jpiv <jjpizzo4@gmail.com>
2025-12-11 03:02:36 -0500

    Adds wrangler

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -5,7 +5,7 @@ import { config } from 'dotenv';
 
 config();
 
-const resend = new Resend(process.env.RESEND_API_KEY!);
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export async function POST(req: NextRequest) {
   try {
